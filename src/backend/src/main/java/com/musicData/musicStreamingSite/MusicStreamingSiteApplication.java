899a8553fed1f202a8d9278e8aa4022db5462c43
Jorge <herreraljorge64@gmail.com>
2022-09-12 21:18:38 -0400

    Set up Java for back end work.

---
+++ src/backend/src/main/java/com/musicData/musicStreamingSite/MusicStreamingSiteApplication.java
@@ -0,0 +1,13 @@
+package com.musicData.musicStreamingSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStreamingSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStreamingSiteApplication.class, args);
+	}
+
+}
---
+++ src/backend/src/test/java/com/musicData/musicStreamingSite/MusicStreamingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicData.musicStreamingSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStreamingSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
