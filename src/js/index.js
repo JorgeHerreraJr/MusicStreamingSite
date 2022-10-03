925e5200555e874af495a2dbe42f5ba5407b4fd5
JorgeHerreraJr <herreraljorge64@gmail.com>
2022-10-03 16:18:02 -0400

    api test

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,32 @@
-function mobileNav() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block"
-    }
+const api_url = "https://randomuser.me/api/";
+async function getUser() {
+    const response = await fetch(api_url);
+
+    const data = await response.json();
+    console.log(data.results);
+
+    const user = data.results[0];
+    let {title, first, last} = user.name;
+    let {gender, email, phone} = user;
+    let image = user.picture.large;
+    let image_icon = user.picture.thumbnail;
+    let age = user.dob.age;
+    let {city, state, country} = user.location;
+
+    let fullName = title + ". " + first + " " + last;
+    document.title = fullName;
+
+    document.getElementById("head").innerHTML = fullName;
+    document.getElementById("email").href = "mailto: " + email;
+    document.getElementById("email").innerHTML = email;
+    document.getElementById("phone").href = "tel: " + phone;
+    document.getElementById("phone").innerHTML = phone;
+
+    document.querySelector("#age").textContent = age;
+    document.querySelector("#gender").textContent = gender;
+
+    document.querySelector("#location").textContent = city + ", " + state;
+    document.querySelector("#country").textContent = country;
+
+    getUser();
 }
